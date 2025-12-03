4bc79f750067f0490b0e127e9340e07c262cad31
EmanoelLuiz01 <133930585+EmanoelLuiz01@users.noreply.github.com>
2025-12-02 22:34:58 -0300

    Primeiro commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    
+    let tituloDoJogo : string = "Quebra cabeça"
+</script>
+
+
+<div class="menu-container">
+    <h1>{tituloDoJogo}</h1>
+    <a class="menu" href="/jogar">Jogar</a>
+    <a class="menu" href="/sobre">Sobre</a>
+</div>
---
+++ src/routes/jogar/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+ import { goto } from '$app/navigation'
+
+</script>
+
+<h1>Movimente o personagem 
+    (quadrado cinza) até o 
+    objetivo (quadrado roxo)</h1>
+<a class="menu" href="/">Voltar ao Menu</a>
