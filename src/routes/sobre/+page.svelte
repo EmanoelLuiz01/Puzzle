992b37eccb408abaa21027c8281264b1772650a6
Crislayne67 <cll4@discente.ifpe.edu.br>
2025-12-04 10:44:10 -0300

    alterei o sobre : colocando os nomes e e-mails

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -2,11 +2,21 @@
 
 <img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"/>
 
-<p>
-    Autor: Allan Diego Silva Lima
-</p>
-<p>
-  <a href="allan.lima@igarassu.ifpe.edu.br">allan.lima@igarassu.ifpe.edu.br</a>
-</p>
 
+ 
+<ul>
+<li>Grupo:</li>
+  <li>Crislayne Lopes<br><a href="mailto:cll4@discente.ifpe.edu.br">cll4@discente.ifpe.edu.br</a></li>
+  <li>Emanoel Luiz<br><a href="mailto:els31@discente.ifpe.edu.br">els31@discente.ifpe.edu.br</a></li>
+  <li>Fracylene Oliveira<br><a href="mailto:fos4@discente.ifpe.edu.br">fos4@discente.ifpe.edu.br</a></li>
+  <li>Hevelyn Nenes<br><a href="mailto:hnsf@discente.ifpe.edu.br">hnsf@discente.ifpe.edu.br</a></li>
+  <li>Maria Beatriz<br><a href="mailto:Mbap@discente.ifpe.edu.br">Mbap@discente.ifpe.edu.br</a></li>
+  <li>Ruan Erlynk<br><a href="mailto:Reps1@discente.ifpe.edu.br">Reps1@discente.ifpe.edu.br</a></li>
+  <li>Vinicyus Miguel<br><a href="vmlp@discente.ifpe.edu.br">vmlp@discente.ifpe.edu.br</a></li>
+</ul>
+
+
+
+
+ 
 <a class="menu" href="/">Voltar ao Menu</a>
